4d57f615ef3414c90feb13f85c682f56314505c9
Yamsha <jakub.lacinski@gmail.com>
2020-11-08 21:41:33 +0100

    Add entrypoint starting scheduler for scraper

---
+++ scraper/main.py
@@ -0,0 +1,34 @@
+import asyncio
+import logging
+
+import settings
+from database import recreate_schema
+from scheduler import scheduler
+
+logging.basicConfig(
+    format=(
+        "%(asctime)s.%(msecs)03d [%(levelname)s] %(module)s.%(funcName)s: "
+        "%(message)s"
+    ),
+    datefmt=r"%H:%M:%S",
+)
+if settings.DEBUG:
+    logging.getLogger("apscheduler").setLevel(logging.DEBUG)
+else:
+    logging.getLogger("apscheduler").setLevel(logging.INFO)
+
+
+def run():
+    recreate_schema()
+
+    scheduler.start()
+    try:
+        asyncio.get_event_loop().run_forever()
+    except (KeyboardInterrupt, SystemExit):
+        print("Shutting down. Please wait...")
+        scheduler.shutdown(wait=True)
+        exit(0)
+
+
+if __name__ == "__main__":
+    run()
