80513ecf32eea984dfe20f820bd7561feff06c2b
Yamsha <jakub.lacinski@gmail.com>
2020-11-12 17:42:59 +0100

    [Flask] Add database interface

---
+++ flask-app/database.py
@@ -0,0 +1,25 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+import settings
+from counties import load_counties_from_csv
+from models import Base
+
+engine = create_engine(settings.DB_CONNECTION_URI)
+session = sessionmaker(bind=engine)()
+
+
+def recreate_schema():
+    Base.metadata.create_all(bind=engine, checkfirst=True)
+
+    counties = load_counties_from_csv()
+    for county in counties:
+        session.merge(county)
+
+    session.commit()
+    return True
+
+
+if __name__ == "__main__":
+    recreate_schema()
+    exit(0)
