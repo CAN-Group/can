8016e24bf77c9bee37af6890d8d1e9e4607250de
Yamsha <jakub.lacinski@gmail.com>
2020-11-12 17:43:18 +0100

    [Flask] Add entrypoint for starting flask-app

---
+++ flask-app/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_cors import CORS
+
+import settings
+from database import session
+from models import County
+
+app = Flask(__name__, static_folder="./static")
+app.config["JSON_AS_ASCII"] = False
+
+CORS(app, origin={settings.FRONTEND_APP_URL})
+
+
+@app.route("/api/v1/counties")
+def get_counties():
+    return {"counties": [county.to_dict() for county in session.query(County).all()]}
+
+
+if __name__ == "__main__":
+    app.run(debug=settings.DEBUG)
