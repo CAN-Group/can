4a3366bfeb8bf7ec5f4ca6c0256e32a64c84d921
Yamsha <jakub.lacinski@gmail.com>
2020-11-13 19:49:00 +0100

    [scraper] Fix missing brackets in function calls

--- scraper/settings.py
+++ scraper/settings.py
@@ -2,9 +2,9 @@ from os import getenv
 
 from dotenv import load_dotenv
 
-load_dotenv
+load_dotenv()
 
-DEBUG = getenv("DEBUG", "").lower in ("true", "1")
+DEBUG = getenv("DEBUG", "").lower() in ("true", "1")
 
 JOBSTORES_URI = getenv("JOBSTORES_URI", "sqlite:///jobs.db")
 DB_CONNECTION_URI = getenv("DB_CONNECTION_URI", "sqlite:///../dev.db")
