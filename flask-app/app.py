c444f8ab008417e43ebc6593d848ba12f3bb0394
Yamsha <jakub.lacinski@gmail.com>
2020-11-29 21:40:38 +0100

    [Flask] Add sitemap listing all routes

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_cors import CORS
 
@@ -9,6 +11,19 @@ app = Flask(__name__, static_folder="./static")
 app.config["JSON_AS_ASCII"] = False
 
 CORS(app, origin={settings.FRONTEND_APP_URL})
+
+
+@app.route("/")
+def sitemap():
+    endpoints = {
+        rule.rule: rule.endpoint
+        for rule in app.url_map.iter_rules()
+        if rule.endpoint != "static"
+    }
+    static_files = os.listdir("./static")
+    for static_file_path in static_files:
+        endpoints[f"/static/{static_file_path}"] = static_file_path
+    return endpoints
 
 
 @app.route("/api/v1/counties")
