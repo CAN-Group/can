2d77502461a062c30a0ef896c3e0642bbd1e0291
Yamsha <jakub.lacinski@gmail.com>
2020-11-29 21:44:36 +0100

    [Flask] Add new route for single county data

--- flask-app/app.py
+++ flask-app/app.py
@@ -12,9 +12,18 @@ CORS(app, origin={settings.FRONTEND_APP_URL})
 
 
 @app.route("/api/v1/counties")
-def get_counties():
+def get_all_counties():
     session = Session()
     return {"counties": [county.to_dict() for county in session.query(County).all()]}
+
+
+@app.route("/api/v1/counties/<string:county_id>")
+def get_county(county_id):
+    session = Session()
+    county = session.query(County).filter_by(id_=county_id).first()
+    if not county:
+        return "county with given id not found", 400
+    return county.to_dict()
 
 
 if __name__ == "__main__":
