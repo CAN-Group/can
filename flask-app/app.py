2bdf3689f594315838f767ef405d89b3a0c1c989
Yamsha <jakub.lacinski@gmail.com>
2020-11-29 22:23:04 +0100

    [Flask] Add flask proxy to brouter

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+import requests
+from flask import Flask, Response
 from flask_cors import CORS
 
 import settings
@@ -15,6 +16,25 @@ CORS(app, origin={settings.FRONTEND_APP_URL})
 def get_counties():
     session = Session()
     return {"counties": [county.to_dict() for county in session.query(County).all()]}
+
+
+@app.route("/api/v1/route/<string:parameters>")
+def proxy_request_to_brouter(parameters):
+    url = settings.ROUTING_APP_URL
+    url = f"{url}?{parameters}"
+    response = requests.get(url)
+    excluded_headers = [
+        "content-encoding",
+        "content-length",
+        "transfer-encoding",
+        "connection",
+    ]
+    headers = [
+        (key, value)
+        for (key, value) in response.headers.items()
+        if key.lower() not in excluded_headers
+    ]
+    return Response(response.content, response.status_code, headers)
 
 
 if __name__ == "__main__":
