973c8a043fa2ebbee4820533176705b4855d68b8
maciejczaja <57909287+maciejczaja@users.noreply.github.com>
2020-12-20 21:41:28 +0100

    Add "today" scrape

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -1,25 +1,48 @@
 from apscheduler.triggers.cron import CronTrigger
-
 from database import DBSession
+from bs4 import BeautifulSoup
+import settings
+import requests
+import pandas as pd
+from datetime import datetime
+import models
+
+
+def get_date_from_uri(uri):
+    r = requests.get(uri)
+    date_str = r.headers["date"]
+    format_date_str = "%a, %d %b %Y %H:%M:%S %Z"
+    return datetime.strptime(date_str, format_date_str)
+
+
+def read_csv_from_uri(uri):
+    df = pd.read_csv(uri, encoding="cp1250", sep=";")
+    return df.values.tolist()
+
+
+def get_uri():
+    req = requests.get(settings.MZ_TODAY_URI)
+    soup = BeautifulSoup(req.text, "lxml")
+    link = soup.find_all("a", {"class": "file-download"})[1]
+    return link["href"]
 
 
 def scrape():
-    # TODO scrape
-    pass
-    # example of adding to db:
-    # db_session = DBSession()
-    # cases = ...
-    # for case in cases:
-    #     cases_record = CasesRecord(
-    #         county_id=case.x,
-    #         updated=case.y,
-    #         number_of_cases=case.z
-    #     )
-    #     session.merge(cases_record)
-    # session.commit()
+    db_session = DBSession()
+
+    uri = get_uri()
+    cases = read_csv_from_uri(uri)
+    date = get_date_from_uri(uri)
+
+    for case in cases:
+        cases_record = models.CasesRecord(
+            county_id=case[7], updated=date.date(), number_of_cases=case[2]
+        )
+        db_session.merge(cases_record)
+    db_session.commit()
 
 
-trigger = CronTrigger(hour="00,12", minutes="0")
+trigger = CronTrigger(hour="00,12", minute="0")
 
 job = {
     "func": scrape,
--- scraper/settings.py
+++ scraper/settings.py
@@ -8,3 +8,11 @@ DEBUG = getenv("DEBUG", "").lower() in ("true", "1")
 
 JOBSTORES_URI = getenv("JOBSTORES_URI", "sqlite:///jobs.db")
 DB_CONNECTION_URI = getenv("DB_CONNECTION_URI", "sqlite:///../dev.db")
+
+MZ_TODAY_URI = getenv(
+    "MZ_TODAY_URI",
+    "https://www.gov.pl/web/koronawirus/mapa-zarazen-koronawirusem-sars-cov-2-powiaty",
+)
+MZ_ARCHIVE_URI = getenv(
+    "MZ_ARCHIVE_URI", "https://www.gov.pl/web/koronawirus/pliki-archiwalne-powiaty"
+)
