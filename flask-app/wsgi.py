f7fe63fccde9b924296a6e487f4f3003780e18d2
Yamsha <jakub.lacinski@gmail.com>
2021-01-22 22:51:11 +0100

    [Flask] Move imports in wsgi.py to top of file
    - before, gunicorn would not start the app
    - reverts change from 9c5a5a67d23665543903d002c16252fb608901ca

--- flask-app/wsgi.py
+++ flask-app/wsgi.py
@@ -1,6 +1,6 @@
-if __name__ == "__main__":
-    from app import app
-    from database import recreate_schema
+from app import app
+from database import recreate_schema
 
+if __name__ == "__main__":
     recreate_schema()
     app.run()
